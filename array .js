723804dd31047ca6fceebbd80ecf5bf6236ad27b
Anildeepsingh <anildeepsingh52@gmail.com>
2022-07-31 12:29:49 +0530

    2nd dimensional array

--- array .js
+++ array .js
@@ -2,5 +2,13 @@
 
 var colors = ["red", "blue", "green"];
 for (var i = 0; i < colors.length; i++) {
-  console.log(colors[i]);
+  //   console.log(colors[i]);
 }
+
+// js array two dimensional
+
+// declaration of a two-dimensional array
+// 5 is the number of rows and 4 is the number of columns.
+const matrix = new Array(5).fill(0).map(() => new Array(4).fill(0));
+
+console.log(matrix[0][0]); // 0
