85e856274b7c3983458f08c3fe8129a5b4d9596a
Anildeepsingh <anildeepsingh52@gmail.com>
2022-07-31 12:32:33 +0530

    array

--- array .js
+++ array .js
@@ -1,14 +1,24 @@
-// one dimensional array in javascript
+//1:- javascript array
+
+//create an array like so:
+var colors = ["red", "blue", "green"];
+
+//you can loop through an array like this:
+for (var i = 0; i < colors.length; i++) {
+  //   console.log(colors[i]);
+}
+
+//2:- one dimensional array in javascript
 
 var colors = ["red", "blue", "green"];
 for (var i = 0; i < colors.length; i++) {
   //   console.log(colors[i]);
 }
 
-// js array two dimensional
+//3:- js array two dimensional
 
 // declaration of a two-dimensional array
 // 5 is the number of rows and 4 is the number of columns.
 const matrix = new Array(5).fill(0).map(() => new Array(4).fill(0));
 
-console.log(matrix[0][0]); // 0
+// console.log(matrix[0][0]); // 0
